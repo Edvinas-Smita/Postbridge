c98f287011aba9c4c5cdbbc4c56e7d88140af6e4
Justina Jurėnaitė <justinaju@gmail.com>
2018-10-18 22:14:39 +0300

    1. favicon.ico added 2. Title has been changed 3. Grid to LoginForm added

--- frontend/src/components/LoginForm/LoginForm.js
+++ frontend/src/components/LoginForm/LoginForm.js
@@ -1,30 +1,47 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button/Button'
 import OutlinedInput from '@material-ui/core/OutlinedInput/OutlinedInput'
+import Grid from '@material-ui/core/Grid/Grid'
 
 class LoginForm extends Component {
     render() {
         return (
-            <div className="App">
-                <OutlinedInput
-                    placeholder="Email"
-                    margin="normal"
-                    type="text"
-                    fullWidth
-                />
-                <OutlinedInput
-                    placeholder="Password"
-                    margin="normal"
-                    type="password"
-                    fullWidth
-                />
-                <Button 
-                    variant = "contained" 
-                    color="primary"
-                >
-                    Sign In
-                </Button>
-            </div>
+
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="center"
+                style={{minHeight: '100vh'}}
+            >
+                <Grid item xs="auto" style={{ padding: 8}}>
+                    <OutlinedInput
+                        placeholder="Email"
+                        margin="dense"
+                        type="text"
+                        fullWidth
+                        labelWidth={0}
+                    />
+                </Grid>
+                <Grid item xs="auto" style={{ padding: 8}}>
+                    <OutlinedInput
+                        placeholder="Password"
+                        margin="dense"
+                        type="password"
+                        fullWidth
+                        labelWidth={0}
+                    />
+                </Grid>
+                <Grid item xs="auto" style={{ padding: 8}}>
+                    <Button 
+                        variant = "contained" 
+                        color="primary"
+                    >
+                        Sign In
+                    </Button>
+                </Grid>
+            </Grid> 
         );
     }
 }

36a09bcd0d5b09848f1c200c1b735f5a4140a879
Justina Jurėnaitė <justinaju@gmail.com>
2018-10-21 20:53:33 +0300

    Added backgroud picture

--- frontend/src/containers/LoginForm/LoginForm.js
+++ frontend/src/containers/LoginForm/LoginForm.js
@@ -1,36 +1,48 @@
 import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
 import Button from '@material-ui/core/Button/Button'
-import OutlinedInput from '@material-ui/core/OutlinedInput/OutlinedInput'
+import TextField from '@material-ui/core/TextField/TextField'
 import Grid from '@material-ui/core/Grid/Grid'
+import  BackgroundImage from './background.png'
+
+const gridStyles = {
+    minHeight: '100vh',
+    backgroundImage: 'url('+BackgroundImage+')',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+}
 
 class LoginForm extends Component {
     render() {
         return (
-
             <Grid
                 container
                 spacing={0}
                 direction="column"
                 alignItems="center"
                 justify="center"
-                style={{minHeight: '100vh'}}
+                style={gridStyles}
             >
                 <Grid item xs="auto" style={{ padding: 8}}>
-                    <OutlinedInput
+                    <TextField
                         placeholder="Email"
                         margin="dense"
                         type="text"
+                        variant="outlined"
                         fullWidth
-                        labelWidth={0}
+                        style={{backgroundColor:"white"}}
                     />
                 </Grid>
                 <Grid item xs="auto" style={{ padding: 8}}>
-                    <OutlinedInput
+                    <TextField
                         placeholder="Password"
                         margin="dense"
                         type="password"
+                        variant="outlined"
                         fullWidth
-                        labelWidth={0}
+                        style={{backgroundColor:"white"}}
                     />
                 </Grid>
                 <Grid item xs="auto" style={{ padding: 8}}>

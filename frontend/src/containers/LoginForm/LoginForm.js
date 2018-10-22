a007b0b2d6a9c58a966a8226062f53c0beb6e471
Justina Jurėnaitė <justinaju@gmail.com>
2018-10-22 12:25:08 +0300

    added missing design elements

--- frontend/src/containers/LoginForm/LoginForm.js
+++ frontend/src/containers/LoginForm/LoginForm.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button/Button'
 import TextField from '@material-ui/core/TextField/TextField'
 import Grid from '@material-ui/core/Grid/Grid'
 import  BackgroundImage from './background.png'
 import { Typography } from '@material-ui/core'
+import { withStyles } from "@material-ui/core/styles";
 import "./LoginForm.css"
 
 const gridStyles = {
@@ -14,59 +15,72 @@ const gridStyles = {
     backgroundPosition: 'center',
 }
 
-class LoginForm extends Component {
-    render() {
-        return (
-            <Grid
-                container
-                spacing={0}
-                direction="column"
-                alignItems="center"
-                justify="center"
-                style={gridStyles}
+const styles = {
+    textField: {
+        width:"400px"
+    },
+    input: {
+        backgroundColor: "white"
+    },
+    typografy: {
+        color: "white"
+    },
+  };
+
+function LoginForm(props){
+    const { classes } = props;
+
+    return (
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            style={gridStyles}
+        >
+            <Typography 
+                variant="h5"
+                className={classes.typografy}
             >
-                <Typography 
-                    variant="h5"
-                    style={{color:"white"}}>
-                    Sign in to your account
-                </Typography>
-                <Grid>
-                    <a href="javascript:void(0)" className="active">Sign In</a>
-                    <a href="javascript:void(0)">Sign Up</a>
-                    <a href="javascript:void(0)">Forgot password?</a>
-                </Grid>
-                <Grid item xs="auto" style={{ padding: 8}}>
-                    <TextField
-                        placeholder="Email"
-                        margin="dense"
-                        type="text"
-                        variant="outlined"
-                        fullWidth
-                        style={{backgroundColor:"white"}}
-                    />
-                </Grid>
-                <Grid item xs="auto" style={{ padding: 8}}>
-                    <TextField
-                        placeholder="Password"
-                        margin="dense"
-                        type="password"
-                        variant="outlined"
-                        fullWidth
-                        style={{backgroundColor:"white"}}
-                    />
-                </Grid>
-                <Grid item xs="auto" style={{ padding: 8}}>
-                    <Button 
-                        variant = "contained" 
-                        color="primary"
-                        style={{textTransform: "none"}}
-                    >
-                        Sign In
-                    </Button>
-                </Grid>
-            </Grid> 
-        );
-    }
+                Sign in to your account
+            </Typography>
+            <Grid item xs={12} style={{ width:"400px", marginTop:"30px"}}>
+                <a href="javascript:void(0)" className="active">Sign In</a>
+                <a href="javascript:void(0)" style={{"padding":"10px"}}>Sign Up</a>
+                <a href="javascript:void(0)" style={{"float":"right", textDecoration:"underline"}}>Forgot password?</a>
+            </Grid>
+            <Grid item xs={12} style={{ padding: 8}}>
+                <TextField
+                    placeholder="Email"
+                    margin="dense"
+                    type="text"
+                    variant="outlined"
+                    className={classes.textField}
+                    inputProps={{className: classes.input}}
+                />
+            </Grid>
+            <Grid item xs={12} style={{ padding: 8}}>
+                <TextField
+                    placeholder="Password"
+                    margin="dense"
+                    type="password"
+                    variant="outlined"
+                    className={classes.textField}
+                    inputProps={{className: classes.input}}
+                />
+            </Grid>
+            <Grid item xs={12} style={{ padding: 8}}>
+                <Button 
+                    variant = "contained" 
+                    color="primary"
+                    style={{textTransform: "none", width:"400px"}}
+                >
+                    Sign In
+                </Button>
+            </Grid>
+        </Grid> 
+    );
 }
 
-export default LoginForm;
+export default withStyles(styles)(LoginForm);

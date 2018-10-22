e88735ac912360a8fee0b9ae803da52bb7d2094b
Martynas Sinkievič <tasmartynas@gmail.com>
2018-10-22 17:41:52 +0300

    Add store (main redux component) to frontend base

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
 import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createMuiTheme, MuiThemeProvider, CssBaseline } from '@material-ui/core';
 import blue from '@material-ui/core/colors/blue';
+import store from './state-management/store/storeFactory';
 import './index.css';
 
 const theme = createMuiTheme({
@@ -17,11 +19,13 @@ const theme = createMuiTheme({
 });
 
 ReactDOM.render((
-    <BrowserRouter>
-        <MuiThemeProvider theme={theme}>
-            <CssBaseline />
-            <App />
-        </MuiThemeProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <MuiThemeProvider theme={theme}>
+                <CssBaseline />
+                <App />
+            </MuiThemeProvider>
+        </BrowserRouter>
+    </Provider>
 ), document.getElementById('root'));
 registerServiceWorker();

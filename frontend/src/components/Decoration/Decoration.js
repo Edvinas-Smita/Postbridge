a1f9a3ce82b93f4d5cd2ba74688dc5f41dd657cd
DESKTOP-9NGHTCS\Gabija <gabija.savu@gmail.com>
2018-10-24 22:15:51 +0300

    Added parcel list page

---
+++ frontend/src/components/Decoration/Decoration.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Decoration.css';
+
+const parcelListDeco = (props) => (
+    <div className="Decoration">
+        
+    </div>
+);
+
+export default parcelListDeco;

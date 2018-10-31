d096df8d9c44f1cdd3106f3b20fa335a43779343
Sleper <saliamonasalgirdas@gmail.com>
2018-10-31 11:46:27 +0200

    sort on the 3 fields, with some helper functions for rendering out the parcels

--- frontend/src/components/Parcel/Parcel.js
+++ frontend/src/components/Parcel/Parcel.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import './Parcel.css';
+import {formatWeight} from '../../helpers';
+import { STATUS } from '../../helpers';
+
 
 const parcel = (props) => (
     <div className="Parcel">
             <div>{props.fromPoint}</div>
             <div>{props.toPoint}</div>
-            <div>{props.status}</div>
+            <div>{STATUS[props.status]}</div>
             <div>{props.description}</div> 
-            <div>{props.weight}</div> 
-            <div>{props.created}</div>
+            <div>{formatWeight(props.weight)}</div> 
+            <div>{props.created.slice(0, 10)}</div>
             <div>{props.delivered}</div>
             <div>{props.recipient}</div>
             <div>{props.courier}</div>
---
+++ frontend/src/helpers.js
@@ -0,0 +1,10 @@
+export function formatWeight(grams) {
+  return (grams/1000) + " kg"
+}
+
+export const STATUS = {
+  1: 'Open',
+  2: 'Picked up',
+  3: 'On the way',
+  4: 'Delivered'
+}

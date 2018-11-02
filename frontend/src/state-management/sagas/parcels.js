e83b9176a81c292d56ce424155f16cf2e9b8abd2
Martynas Sinkievič <tasmartynas@gmail.com>
2018-11-02 17:16:13 +0200

    Get parcel list from json-server instead of hardcode

--- frontend/src/state-management/sagas/parcels.js
+++ frontend/src/state-management/sagas/parcels.js
@@ -5,17 +5,13 @@ import { initParcelsError, initParcelsSuccess } from '../actions/parcels';
 
 function* initParcels() {
     try {
-        const parcels = [
-            { fromPoint: 'Toronto', toPoint: 'Kaunas', status: 'Open', description: 'Books', weight: '1.5kg', created: '21.09.2018', delivered: '', recipient: 'me', courier: 'Arthur Jameson'},
-            { fromPoint: 'Vilnius', toPoint: 'London', status: 'Picked up', description: 'Electronic goods', weight: '3kg', created: '21.09.2018', delivered: '', recipient: 'Patrick Strongwell', courier: 'Monica Wilde'},
-            { fromPoint: 'Chicago', toPoint: 'Kaunas', status: 'Delivered', description: 'Home appliances', weight: '2.85kg', created: '21.09.2018', delivered: '21.09.2018', recipient: 'Vytautas Stankevicius', courier: 'Matt Cox'},
-            { fromPoint: 'Kaunas', toPoint: 'Vilnius', status: 'On the way', description: 'Electronic goods', weight: '0.5kg', created: '21.09.2018', delivered: '', recipient: 'Lindsay Smith', courier: 'Carolyne James'},
-            { fromPoint: 'Toronto', toPoint: 'Kaunas', status: 'Open', description: 'Books', weight: '1.25kg', created: '21.09.2018', delivered: '', recipient: 'me', courier: 'Arthur Jameson'},
-            { fromPoint: 'Toronto', toPoint: 'Kaunas', status: 'Open', description: 'Books', weight: '1.5kg', created: '21.09.2018', delivered: '', recipient: 'me', courier: 'Arthur Jameson'},
-            { fromPoint: 'Vilnius', toPoint: 'London', status: 'Picked up', description: 'Electronic goods', weight: '3kg', created: '21.09.2018', delivered: '', recipient: 'Patrick Strongwell', courier: 'Monica Wilde'},
-            { fromPoint: 'Chicago', toPoint: 'Kaunas', status: 'Delivered', description: 'Home appliances', weight: '2.85kg', created: '21.09.2018', delivered: '21.09.2018', recipient: 'Vytautas Stankevicius', courier: 'Matt Cox'},
-            { fromPoint: 'Kaunas', toPoint: 'Vilnius', status: 'On the way', description: 'Electronic goods', weight: '0.5kg', created: '21.09.2018', delivered: '', recipient: 'Lindsay Smith', courier: 'Carolyne James'},
-        ];
+        let parcels = [];
+        yield fetch("http://localhost:5000/Parcels").then(response => {
+            return response.json();
+        }).then(data => {
+            parcels = Object.values(data);
+        });
+
         yield put(initParcelsSuccess(parcels));
     } catch (e){
         yield put(initParcelsError(e));

7da69d45c3f4b9a0bcdbdec0fddb5d5f3da88303
Martynas Sinkievič <tasmartynas@gmail.com>
2018-11-05 22:44:28 +0200

    Add delete action in redux

--- frontend/src/state-management/sagas/parcels.js
+++ frontend/src/state-management/sagas/parcels.js
@@ -1,6 +1,6 @@
-import { put, all, takeLatest } from 'redux-saga/effects';
-import { GET_PARCELS } from '../constants/parcels';
-import { getParcelsError, getParcelsSuccess } from '../actions/parcels';
+import { put, all, takeLatest, takeEvery } from 'redux-saga/effects';
+import { GET_PARCELS, DELETE_PARCEL } from '../constants/parcels';
+import { getParcelsError, getParcelsSuccess, deleteParcelSuccess, deleteParcelError } from '../actions/parcels';
 
 
 function* getParcels() {
@@ -18,9 +18,23 @@ function* getParcels() {
     }
 }
 
+function* deleteParcel(action) {
+    try {
+        const { id } = action;
+        yield fetch("http://localhost:5000/Parcels/" + id, {
+            method: 'delete',
+        }).then(response => response.json());
+
+        yield put(deleteParcelSuccess(id));
+    } catch(e) {
+        yield put(deleteParcelError(e));
+    }
+}
+
 function* parcelsSaga() {
     yield all([
         takeLatest(GET_PARCELS, getParcels),
+        takeEvery(DELETE_PARCEL, deleteParcel),
     ]);
 }
 

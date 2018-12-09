89b99ca01d1ed04725f5d1ab5170e67192e6fd80
Justina Jurėnaitė <justina.jurenaite@ibapplications.com>
2018-12-09 23:50:25 +0200

    inserts changed to have posibility insert date without recreating DB objects

--- backend/database/insert_data.sql
+++ backend/database/insert_data.sql
@@ -3,28 +3,30 @@ delete from parcels;
 delete from users;
 delete from locations;
 
-INSERT INTO users(first_name, last_name, email, salt, hash) --Passwords are '{first_name}Password123'
+INSERT INTO users(id, first_name, last_name, email, salt, hash) --Passwords are '{first_name}Password123'
 VALUES
-  ('Homer',	'Simpson',	'homer@the.simpsons',	'$2a$06$rGm395JtzvBqNLdg0wyph.',	'$2a$06$rGm395JtzvBqNLdg0wyph.klr4EpUck5pbe3CPRzLpd6YRHraWCre'),
-  ('Marge',	'Simpson',	'marge@the.simpsons',	'$2a$06$mtKNtIEkKxRLFwUWdt8qv.',	'$2a$06$mtKNtIEkKxRLFwUWdt8qv.Ik1uFilnRFVPFLJ8TiaRHZLx.5vK6kC'),
-  ('Bart',	'Simpson',	'bartholomew@the.simpsons',	'$2a$06$0AGeyxAuLdJK3GL34WkB6u',	'$2a$06$0AGeyxAuLdJK3GL34WkB6uSLIk.SR5eBb/IVcVG3k9NRZkF19hmAC'),
-  ('Lisa',	'Simpson',	'lisa@the.simpsons',	'$2a$06$jZlO1Jt0KiVnW3BsHMvu2.',	'$2a$06$jZlO1Jt0KiVnW3BsHMvu2.0c5Z3MsjgSbpX53DGi86o3QhqwEUFFm'),
-  ('Maggie',	'Simpson',	'maggie@the.simpsons',	'$2a$06$QJpl5AIlXPn2MAii5Aw6fe',	'$2a$06$QJpl5AIlXPn2MAii5Aw6febeJ73Ow6REcI.2Graz1aB7z.Feyb5C.'),
-  ('Abraham',	'Simpson',	'abraham@the.simpsons',	'$2a$06$SCm86cfKHLlBwLiwAHX5v.',	'$2a$06$SCm86cfKHLlBwLiwAHX5v.QUhPcrGSiavbeM0t1nYzhRgMBnko4dS'),
-  ('Patty',	'Bouvier',	'patty@the.simpsons',	'$2a$06$D2wTMcC5.JOf8fQc12Tl4e',	'$2a$06$D2wTMcC5.JOf8fQc12Tl4eRo56n9P3Hf6FSP.q5JAo/e.cnXpbtJy'),
-  ('Selma',	'Bouvier',	'selma@the.simpsons',	'$2a$06$IL2XK5Bqrqah23LA8Tfy5u',	'$2a$06$IL2XK5Bqrqah23LA8Tfy5u4sKKLpNZXe4MZV7lj.ChvxQH.JfcJ5i'),
-  ('Millhouse',	'Mussolini Van Houten',	'millhouse@the.simpsons',	'$2a$06$x32W5oNrjx7BEoH51w21mO',	'$2a$06$x32W5oNrjx7BEoH51w21mOTva2klovmfSuihgI8pYJyI0.9F0x8JG'),
-  ('Ralph',	'Wiggum',	'ralph@the.simpsons',	'$2a$06$Y87NUQc0qbzxjF.y22vF6O',	'$2a$06$Y87NUQc0qbzxjF.y22vF6Omms8GWucod/k6MoyVC3t3NUSjL.HEUW'),
-  ('Nelson',	'Muntz',	'nelson@the.simpsons',	'$2a$06$0bdAmYpDOedCiRNE4.qt0O',	'$2a$06$0bdAmYpDOedCiRNE4.qt0ONOTUOpeVPE./nJI0Yklt8qYhyWmVwX6'),
-  ('Moe',	'Szyslak',	'moe@the.simpsons',	'$2a$06$7lwgJslV6v6TLHPg6G9RSO',	'$2a$06$7lwgJslV6v6TLHPg6G9RSOpWnDBHGgrSbSkNRbU3lS6N4a3Is8Uxa'),
-  ('Ned',	'Flanders',	'ned@the.simpsons',	'$2a$06$I8jzTpl32Hcidxey7SEqQO',	'$2a$06$I8jzTpl32Hcidxey7SEqQO4LhWyS/4RG9CPanfnQj3Fi00CAwGj1S'),
-  ('Principal',	'Skinner!!!!!!',	'principal@the.simpsons',	'$2a$06$miRio0E69AIFU.T2n9rNKu',	'$2a$06$miRio0E69AIFU.T2n9rNKuY1.3sw3s8UiNPYWe4H.1nMiRQ7UTkHe'),
-  ('Charles',	'Montgomery Burns',	'charles@the.simpsons',	'$2a$06$4TZYItjHnG/D30TKZvH1ju',	'$2a$06$4TZYItjHnG/D30TKZvH1jumx6f7RctFTp34bWMkE86CFDK2ijWy0K'),
-  ('Waylon',	'Smithers',	'waylon@the.simpsons',	'$2a$06$Q/Qk7cYguUCJ32QVnPKuku',	'$2a$06$Q/Qk7cYguUCJ32QVnPKukuCAhgdvW2yf76KyldhewrCOeWwwxJx1.'),
-  ('TEST',	'BIG PASSWORD',	'test@big.password',	'$2a$06$1kvE4e7.5GZDPCd/SAx44.',	'$2a$06$1kvE4e7.5GZDPCd/SAx44.avsmvpaQW1rPFdG5Rm0LYsw4lrJbBA6');
+  (1, 'Homer',	'Simpson',	'homer@the.simpsons',	'$2a$06$rGm395JtzvBqNLdg0wyph.',	'$2a$06$rGm395JtzvBqNLdg0wyph.klr4EpUck5pbe3CPRzLpd6YRHraWCre'),
+  (2, 'Marge',	'Simpson',	'marge@the.simpsons',	'$2a$06$mtKNtIEkKxRLFwUWdt8qv.',	'$2a$06$mtKNtIEkKxRLFwUWdt8qv.Ik1uFilnRFVPFLJ8TiaRHZLx.5vK6kC'),
+  (3, 'Bart',	'Simpson',	'bartholomew@the.simpsons',	'$2a$06$0AGeyxAuLdJK3GL34WkB6u',	'$2a$06$0AGeyxAuLdJK3GL34WkB6uSLIk.SR5eBb/IVcVG3k9NRZkF19hmAC'),
+  (4, 'Lisa',	'Simpson',	'lisa@the.simpsons',	'$2a$06$jZlO1Jt0KiVnW3BsHMvu2.',	'$2a$06$jZlO1Jt0KiVnW3BsHMvu2.0c5Z3MsjgSbpX53DGi86o3QhqwEUFFm'),
+  (5, 'Maggie',	'Simpson',	'maggie@the.simpsons',	'$2a$06$QJpl5AIlXPn2MAii5Aw6fe',	'$2a$06$QJpl5AIlXPn2MAii5Aw6febeJ73Ow6REcI.2Graz1aB7z.Feyb5C.'),
+  (6, 'Abraham',	'Simpson',	'abraham@the.simpsons',	'$2a$06$SCm86cfKHLlBwLiwAHX5v.',	'$2a$06$SCm86cfKHLlBwLiwAHX5v.QUhPcrGSiavbeM0t1nYzhRgMBnko4dS'),
+  (7, 'Patty',	'Bouvier',	'patty@the.simpsons',	'$2a$06$D2wTMcC5.JOf8fQc12Tl4e',	'$2a$06$D2wTMcC5.JOf8fQc12Tl4eRo56n9P3Hf6FSP.q5JAo/e.cnXpbtJy'),
+  (8, 'Selma',	'Bouvier',	'selma@the.simpsons',	'$2a$06$IL2XK5Bqrqah23LA8Tfy5u',	'$2a$06$IL2XK5Bqrqah23LA8Tfy5u4sKKLpNZXe4MZV7lj.ChvxQH.JfcJ5i'),
+  (9, 'Millhouse',	'Mussolini Van Houten',	'millhouse@the.simpsons',	'$2a$06$x32W5oNrjx7BEoH51w21mO',	'$2a$06$x32W5oNrjx7BEoH51w21mOTva2klovmfSuihgI8pYJyI0.9F0x8JG'),
+  (10, 'Ralph',	'Wiggum',	'ralph@the.simpsons',	'$2a$06$Y87NUQc0qbzxjF.y22vF6O',	'$2a$06$Y87NUQc0qbzxjF.y22vF6Omms8GWucod/k6MoyVC3t3NUSjL.HEUW'),
+  (11, 'Nelson',	'Muntz',	'nelson@the.simpsons',	'$2a$06$0bdAmYpDOedCiRNE4.qt0O',	'$2a$06$0bdAmYpDOedCiRNE4.qt0ONOTUOpeVPE./nJI0Yklt8qYhyWmVwX6'),
+  (12, 'Moe',	'Szyslak',	'moe@the.simpsons',	'$2a$06$7lwgJslV6v6TLHPg6G9RSO',	'$2a$06$7lwgJslV6v6TLHPg6G9RSOpWnDBHGgrSbSkNRbU3lS6N4a3Is8Uxa'),
+  (13, 'Ned',	'Flanders',	'ned@the.simpsons',	'$2a$06$I8jzTpl32Hcidxey7SEqQO',	'$2a$06$I8jzTpl32Hcidxey7SEqQO4LhWyS/4RG9CPanfnQj3Fi00CAwGj1S'),
+  (14, 'Principal',	'Skinner!!!!!!',	'principal@the.simpsons',	'$2a$06$miRio0E69AIFU.T2n9rNKu',	'$2a$06$miRio0E69AIFU.T2n9rNKuY1.3sw3s8UiNPYWe4H.1nMiRQ7UTkHe'),
+  (15, 'Charles',	'Montgomery Burns',	'charles@the.simpsons',	'$2a$06$4TZYItjHnG/D30TKZvH1ju',	'$2a$06$4TZYItjHnG/D30TKZvH1jumx6f7RctFTp34bWMkE86CFDK2ijWy0K'),
+  (16, 'Waylon',	'Smithers',	'waylon@the.simpsons',	'$2a$06$Q/Qk7cYguUCJ32QVnPKuku',	'$2a$06$Q/Qk7cYguUCJ32QVnPKukuCAhgdvW2yf76KyldhewrCOeWwwxJx1.'),
+  (17, 'TEST',	'BIG PASSWORD',	'test@big.password',	'$2a$06$1kvE4e7.5GZDPCd/SAx44.',	'$2a$06$1kvE4e7.5GZDPCd/SAx44.avsmvpaQW1rPFdG5Rm0LYsw4lrJbBA6');
 --  TEST registered password is 'A Very Big And Long Password With Spaces And A Max Signed Integer Value Number 2147483647' (89 chars),
 --  but the algorithm used only supports 72 so last 17 chars are ignored (starting at 'Number'), meaning the important part of the password is
 --  'A Very Big And Long Password With Spaces And A Max Signed Integer Value '
+
+select setval('users_id_seq', (select max(id) from users)+1);
 
 insert into locations (name) values ('Vilnius');
 insert into locations (name) values ('Kaunas');
